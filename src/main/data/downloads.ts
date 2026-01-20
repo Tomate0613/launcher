b9e83e8a338be4bdbbc15672fc95e4ba0431021b
Tomate0613 <69756782+Tomate0613@users.noreply.github.com>
2026-01-20 21:17:44 +0100

    retry downloads

--- src/main/data/downloads.ts
+++ src/main/data/downloads.ts
@@ -1,10 +1,24 @@
 import { DownloadManager } from 'tomate-launcher-core';
 // import { log } from '../../common/logging/log';
 import { invoke } from '../api';
+import axiosRetry from 'axios-retry';
+import axios from 'axios';
+import { Agent as HttpAgent } from 'node:http';
+import { Agent as HttpsAgent } from 'node:https';
 
 // const logger = log('downloads');
+//
+//
 
-export const downloadManager = new DownloadManager();
+const httpAgent = new HttpAgent({ keepAlive: true, maxSockets: 5 });
+const httpsAgent = new HttpsAgent({ keepAlive: true, maxSockets: 5 });
+const api = axios.create({
+  httpAgent,
+  httpsAgent,
+});
+axiosRetry(api);
+
+export const downloadManager = new DownloadManager(api);
 // This is too verbose even for verbose logging
 // downloadManager.on('debug', (msg) => logger.verbose(msg));
 
