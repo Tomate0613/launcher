3f7e9a4184bc5ec7ce907801f8f60b1b6318369f
Tomate0613 <69756782+Tomate0613@users.noreply.github.com>
2026-01-21 17:31:21 +0100

    add labwc script

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -2,5 +2,4 @@
 
 pnpm build:linux --linux flatpak || exit 1
 sudo flatpak install 'dist/Tomate Launcher-1.0.0-x86_64.flatpak' || exit 1
-labwc -S "flatpak run dev.doublekekse.launcher" || echo LabWC not installed
-
+./labwc.sh
---
+++ scripts/labwc.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+labwc -S "flatpak run dev.doublekekse.launcher" || echo LabWC not installed
+
