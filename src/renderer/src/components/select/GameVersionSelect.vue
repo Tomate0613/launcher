060efbb8b82f5655b99c6d77b6dc39b4bd483d2c
Tomate0613 <69756782+Tomate0613@users.noreply.github.com>
2026-02-15 18:38:34 +0100

    fix game version select with snapshots

--- src/renderer/src/components/select/GameVersionSelect.vue
+++ src/renderer/src/components/select/GameVersionSelect.vue
@@ -2,13 +2,30 @@
 import { computedAsync } from '@vueuse/core';
 import { LoaderId } from 'tomate-loaders';
 import { computed, ref } from 'vue';
+import { log } from '../../../../common/logging/log';
+
+const logger = log('game-version-select');
 
 const props = defineProps<{ loader: LoaderId }>();
 const model = defineModel<string>();
 
 const versions = computedAsync(
-  async () => await window.api.invoke('gameVersions', props.loader),
-  [],
+  async () => {
+    const loaders = await window.api.invoke('gameVersions', props.loader);
+
+    if (model.value) {
+      const loader = loaders.find((l) => l.version == model.value);
+
+      if (!loader) {
+        logger.warn(`Game version "${model.value}" not included in list`);
+      }
+
+      showSnapshots.value = loader ? !loader.stable : false;
+    }
+
+    return loaders;
+  },
+  model.value ? [{ version: model.value, stable: true }] : [],
 );
 const showSnapshots = ref(false);
 
