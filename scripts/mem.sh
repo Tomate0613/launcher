5105e9a33578abc2c9ada9ca61188bf429c7ff9a
Tomate0613 <69756782+Tomate0613@users.noreply.github.com>
2026-02-26 23:30:38 +0100

    allow for passing in PID in mem script

--- scripts/mem.sh
+++ scripts/mem.sh
@@ -9,7 +9,7 @@ get_children() {
 }
 
 # This is specific to how we package with nix
-ROOT_PID=$(pgrep -fo '[e]lectron.*TomateLauncher/app.asar')
+ROOT_PID=${1:-$(pgrep -fo '[e]lectron.*TomateLauncher/app.asar')}
 PIDS=$(get_children "$ROOT_PID" | sed 's/|$//')
 
 smem -c "pid pss" | grep -E "$PIDS" | awk '{sum+=$2} END {print sum/1024 " MB"}'
