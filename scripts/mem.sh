eb92870810c50b5ca6e21f9ff8ecbf1462ae3e7b
Tomate0613 <69756782+Tomate0613@users.noreply.github.com>
2026-02-26 22:52:26 +0100

    add mem.sh script

---
+++ scripts/mem.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env -S nix shell nixpkgs#bash nixpkgs#smem --command bash
+
+get_children() {
+  local pid=$1
+  printf "%s|" "$pid"
+  for child in $(pgrep -P "$pid"); do
+    get_children "$child"
+  done
+}
+
+# This is specific to how we package with nix
+ROOT_PID=$(pgrep -fo '[e]lectron.*TomateLauncher/app.asar')
+PIDS=$(get_children "$ROOT_PID" | sed 's/|$//')
+
+smem -c "pid pss" | grep -E "$PIDS" | awk '{sum+=$2} END {print sum/1024 " MB"}'
