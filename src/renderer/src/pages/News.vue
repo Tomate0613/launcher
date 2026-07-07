af19e5542febf89a64e4cb7a4737930b32b0284b
Tomate0613 <69756782+Tomate0613@users.noreply.github.com>
2026-07-07 17:29:36 +0200

    add copy url contextmenu to news page entries

--- src/renderer/src/pages/News.vue
+++ src/renderer/src/pages/News.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+import ContextMenuWrapper from '../components/ContextMenuWrapper.vue';
+import Icon from '../components/Icon.vue';
 import type { JavaPatchnotesEntry } from '../../../main/browse';
+import { mdiContentCopy } from '@mdi/js';
 
 const javaPatchNotes = await window.api.invoke('getJavaPatchnotes');
 
 function viewEntry(selected: JavaPatchnotesEntry) {
   window.api.invoke('viewJavaPatchnoteEntry', selected.version);
+}
+
+function copy(selected: JavaPatchnotesEntry) {
+  const url = `https://quiltmc.org/en/mc-patchnotes/#${selected.version}`
+  navigator.clipboard.writeText(url);
 }
 </script>
 
@@ -12,13 +20,26 @@ function viewEntry(selected: JavaPatchnotesEntry) {
   <div class="page-header" />
   <div class="page-content">
     <div class="page-scrollable news">
-      <div v-for="entry in javaPatchNotes.entries">
-        <button @click="viewEntry(entry)">
-          <h2>{{ entry.title }}</h2>
-          <img :src="`https://launchercontent.mojang.com/${entry.image.url}`" />
-          <div class="short-text">{{ entry.shortText }}</div>
-        </button>
-      </div>
+      <ContextMenuWrapper v-for="entry in javaPatchNotes.entries">
+        <template v-slot:content>
+          <button @click="viewEntry(entry)" class="entry">
+            <h2>{{ entry.title }}</h2>
+            <img
+              :src="`https://launchercontent.mojang.com/${entry.image.url}`"
+            />
+            <div class="short-text">{{ entry.shortText }}</div>
+          </button>
+        </template>
+        <template v-slot:context-menu>
+          <button
+            class="icon-btn"
+            @click="copy(entry)"
+          >
+            <Icon :path="mdiContentCopy" />
+            Copy URL
+          </button>
+        </template>
+      </ContextMenuWrapper>
     </div>
   </div>
 </template>
@@ -33,7 +54,7 @@ function viewEntry(selected: JavaPatchnotesEntry) {
   grid-template-columns: repeat(auto-fill, minmax(360px, 1fr));
   grid-auto-rows: min-content;
 
-  & button {
+  & button.entry {
     display: flex;
     flex-direction: column;
     height: 100%;
