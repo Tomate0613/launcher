d864175ac079abc241a43d4c531a66b58ba0d36c
Tomate0613 <69756782+Tomate0613@users.noreply.github.com>
2026-09-21 23:31:39 +0200

    remove ability to disable wrapper

--- src/main/data/settings.ts
+++ src/main/data/settings.ts
@@ -28,7 +28,6 @@ export type GeneralModpackOptions = {
 };
 
 export type WrapperOptions = {
-  enabled: boolean;
   reopen: boolean;
   autoClose: boolean;
   sandbox: boolean;
@@ -61,7 +60,6 @@ export class Settings extends Serializable {
   hideFrame: boolean = false;
   @SerializableProperty
   wrapper: WrapperOptions = {
-    enabled: true,
     reopen: true,
     autoClose: false,
     sandbox: false,
@@ -77,7 +75,6 @@ export class Settings extends Serializable {
     switch (version) {
       case '1':
         this.wrapper = {
-          enabled: true,
           reopen: true,
           autoClose: false,
           sandbox: false,
